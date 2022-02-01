496257a3f108fca36fc2c053246b5a7bb66fae68
vinay <ypvinayreddy@gmail.com>
2022-01-31 21:22:25 -0700

    Implemented Auto Complete React js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Input, Card } from 'antd';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [countries, setCountries] = useState([]);
+	const [countryMatch, setCountryMatch] = useState([]);
+
+	useEffect(() => {
+		const loadCountries = async () => {
+			const response = await axios.get('https://restcountries.com/v2/all');
+			setCountries(response.data);
+		};
+		loadCountries();
+	}, []);
+
+	const searchCountries = (text) => {
+		let matches = countries.filter((country) => {
+			const regex = new RegExp(`${text}`, 'gi');
+			console.log('regex', regex);
+			console.log('match', country.name.match(regex));
+
+			return country?.name?.match(regex) || country?.capital?.match(regex);
+		});
+		console.log('matches', matches);
+		setCountryMatch(matches);
+	};
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h2>I am reacting react app autocomplete from scartch</h2>
+				<Input
+					style={{ width: '40%', marginTop: '10px', padding: '12px' }}
+					onChange={(e) => searchCountries(e.target.value)}
+				/>
+				{countryMatch &&
+					countryMatch.map((item, index) => (
+						<div key={index} style={{ mariginLeft: '35%' }}>
+							<Card style={{ width: '50%' }} title={`Country: ${item.name}`}>
+								Capital: {item.capital}
+							</Card>
+						</div>
+					))}
+			</header>
+		</div>
+	);
 }
 
 export default App;
